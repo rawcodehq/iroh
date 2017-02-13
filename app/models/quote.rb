1a163ccbf06e271aaa9f4b8e321575db543e3adb
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-02-13 13:58:12 +0530

    fix bug when data directory is not available

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,13 +8,19 @@ class Quote
   end
 
   def save
-    quotes_file = File.open("./data/quotes.txt", "a")
+    quotes_file = File.open(quotes_file_path, "a")
     quotes_file.puts(@body)
     quotes_file.close
   end
 
   def self.all
-    File.read("./data/quotes.txt").each_line
+    File.read(quotes_file_path).each_line
       .map{|x| Quote.new(x.strip) }
+  end
+
+  DATA_DIR = Rails.root.join("data")
+  FileUtils.mkdir_p(DATA_DIR)
+  def quotes_file_path
+    DATA_DIR.join("quotes.txt").to_s
   end
 end
