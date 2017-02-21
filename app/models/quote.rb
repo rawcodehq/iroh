23818ac2e9f411a4d7e29fcc4d02d97e93e124bf
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-02-21 10:58:53 +0530

    refactor

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,9 @@
 class Quote
+
+  DATA_DIR = Rails.root.join("data")
+  FileUtils.mkdir_p(DATA_DIR)
+  QUOTES_FILE_PATH = DATA_DIR.join("quotes.txt").to_s
+
   def initialize(body)
     @body = body
   end
@@ -8,19 +13,13 @@ class Quote
   end
 
   def save
-    quotes_file = File.open(quotes_file_path, "a")
+    quotes_file = File.open(Quote.QUOTES_FILE_PATH, "a")
     quotes_file.puts(@body)
     quotes_file.close
   end
 
   def self.all
-    File.read(quotes_file_path).each_line
+    File.read(QUOTES_FILE_PATH).each_line
       .map{|x| Quote.new(x.strip) }
-  end
-
-  DATA_DIR = Rails.root.join("data")
-  FileUtils.mkdir_p(DATA_DIR)
-  def quotes_file_path
-    DATA_DIR.join("quotes.txt").to_s
   end
 end
